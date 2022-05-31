16f83580bb2156ae93bb80ca41a9ba2c8afced64
Stephen <stephenswanton@gmail.com>
2022-05-31 14:27:56 +0100

    feat: sanitising description fields.

--- api/place-api.js
+++ api/place-api.js
@@ -1,4 +1,5 @@
 import Boom from "@hapi/boom";
+import sanitizeHtml from "sanitize-html";
 import { db } from "../src/models/db.js";
 import {
   PlaceArray,
@@ -7,7 +8,7 @@ import {
   PlaceSpecPlusWithCategoriesObject,
   IdSpec, PlaceSpecWithCategory, PlaceSpecWithCategoryAndId,
 } from "../src/models/joi-schemas.js";
-import { validationError} from "./logger.js";
+import { validationError } from "./logger.js";
 import { imageStore } from "../src/models/image-store.js";
 
 export const placeApi = {
@@ -27,7 +28,7 @@ export const placeApi = {
     tags: ["api"],
     description: "Get all Places",
     notes: "Returns details of all Places",
-    response: { schema: PlaceArray, failAction: validationError }
+    response: { schema: PlaceArray, failAction: validationError },
   },
 
   findOne: {
@@ -42,7 +43,7 @@ export const placeApi = {
         }
         const returnedCategories = await db.categoryStore.getCategoriesByPlace(place._id);
         place.categories = returnedCategories;
-        console.log(place)
+        console.log(place);
         return place;
       } catch (err) {
         return Boom.serverUnavailable("No PlaceMark with this id");
@@ -52,19 +53,34 @@ export const placeApi = {
     description: "Gets details related to a Place",
     notes: "Returns details of a Place based on the ID provided",
     validate: { params: { id: IdSpec }, failAction: validationError },
-    response: { schema: PlaceSpecPlusWithCategoriesObject, failAction: validationError }
+    response: { schema: PlaceSpecPlusWithCategoriesObject, failAction: validationError },
   },
 
   create: {
     auth: {
       strategy: "jwt",
     },
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       try {
         const place = request.payload;
-        place.createdBy = request.auth.credentials._id
+        if (request.payload.images) {
+          const imageString = request.payload.images;
+          const imageArray = imageString.split(",");
+          imageArray.pop();
+          place.images = imageArray;
+        }
+        if (request.payload.description) {
+          place.description = sanitizeHtml(request.payload.description, {
+              allowedTags: ["b", "i", "em", "ul", "li", "strong", "a"],
+              allowedAttributes: {
+                "a": ["href"],
+              },
+            },
+          );
+        }
+        place.createdBy = request.auth.credentials._id;
         const newPlace = await db.placeStore.addPlace(place);
-        console.log(newPlace)
+        console.log(newPlace);
         if (newPlace) {
           return h.response(newPlace).code(201);
         }
@@ -77,7 +93,7 @@ export const placeApi = {
     description: "Creates a new Place",
     notes: "Adds a new place to the database",
     validate: { payload: PlaceSpecWithCategory, failAction: validationError },
-    response: { schema: PlaceSpecPlus, failAction: validationError }
+    response: { schema: PlaceSpecPlus, failAction: validationError },
   },
 
   saveEdited: {
@@ -87,12 +103,12 @@ export const placeApi = {
     validate: {
       payload: PlaceSpecWithCategoryAndId,
       options: { abortEarly: false },
-      failAction: function (request, h, error) {
+      failAction: function(request, h, error) {
         console.log(error.details);
         return Boom.badRequest("Invalid Query");
       },
     },
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       const updatedPlace = request.payload;
       if (request.payload.images) {
         const imageString = request.payload.images;
@@ -100,11 +116,20 @@ export const placeApi = {
         imageArray.pop();
         updatedPlace.images = imageArray;
       }
-      updatedPlace.createdBy = request.auth.credentials._id
+      if (request.payload.description) {
+        updatedPlace.description = sanitizeHtml(request.payload.description, {
+            allowedTags: ["b", "i", "em", "ul", "li", "strong", "a"],
+            allowedAttributes: {
+              "a": ["href"],
+            },
+          },
+        );
+      }
+      updatedPlace.createdBy = request.auth.credentials._id;
       const updatePlace = await db.placeStore.updatePlace(request.params.id, updatedPlace);
       if (updatePlace.message) {
         const errorDetails = [{ message: updatePlace.message }];
-        console.log(errorDetails)
+        console.log(errorDetails);
         return Boom.badRequest("Invalid Query");
       }
       const originalCategories = await db.categoryStore.getCategoriesByPlace(request.params.id);
@@ -124,7 +149,7 @@ export const placeApi = {
         await db.categoryStore.addPlace(request.params.id, category._id);
       }
       const result = await db.placeStore.getPlaceById(updatedPlace._id);
-      return result
+      return result;
     },
   },
 
@@ -132,11 +157,11 @@ export const placeApi = {
     auth: {
       strategy: "jwt",
     },
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       try {
         const place = await db.placeStore.getPlaceById(request.params.id);
-        console.log("returned place")
-        console.log(place)
+        console.log("returned place");
+        console.log(place);
         if (!place) {
           return Boom.notFound("No PlaceMark with this id");
         }
@@ -156,7 +181,7 @@ export const placeApi = {
     auth: {
       strategy: "jwt",
     },
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       try {
         await db.placeStore.deleteAll();
         return h.response().code(204);
@@ -172,7 +197,7 @@ export const placeApi = {
     auth: {
       strategy: "jwt",
     },
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       try {
         const file = request.payload.imagefile;
         if (Object.keys(file).length > 0) {
@@ -192,5 +217,5 @@ export const placeApi = {
       parse: true,
     },
   },
-  
+
 };
